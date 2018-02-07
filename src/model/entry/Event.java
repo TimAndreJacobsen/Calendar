bc9ac9317b9c09b96043b19da0ed0dc7832f95bb
TimAndreJacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2018-02-07 23:54:33 +0100

    bugfix
    Was not inheriting getTitle

--- src/model/entry/Event.java
+++ src/model/entry/Event.java
@@ -5,14 +5,14 @@ import java.time.LocalDateTime;
 public class Event extends Entry {
 
     private final Label LABEL;
-    private String title;
-
 
     public Event(LocalDateTime date, String title) {
         super(date, title);
         LABEL = Label.EVENT;
     }
 
-
     //Getters
+    public Label getLabel() {
+        return LABEL;
+    }
 }
